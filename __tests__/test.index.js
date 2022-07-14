d4b6a949834fa53f5f9ca063d2a49a7ce2ad504c
Kaplin Vladimir <hawk.hawk30@gmail.com>
2022-07-14 17:09:15 +0300

    test.index.js

--- __tests__/test.index.js
+++ __tests__/test.index.js
@@ -7,7 +7,7 @@ import gendiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('gendiff file1.json, file2.json', () => {
