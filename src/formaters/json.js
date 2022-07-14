b0a6dc1eb65114a86a4011b97e0560ce16a01e64
Kaplin Vladimir <hawk.hawk30@gmail.com>
2022-07-14 22:02:19 +0300

    fix json.js

--- src/formaters/json.js
+++ src/formaters/json.js
@@ -1,25 +1,28 @@
 const json = (data) => {
   const iter = (value) => {
-    const obj = {};
-    value.forEach((el) => {
+    const result = value.reduce((acc, el) => {
       switch (el.type) {
         case 'added':
         case 'removed':
         case 'unchanged':
-          obj[el.name] = { type: el.type, value: el.value };
+          acc[el.name] = { type: el.type, value: el.value };
           break;
         case 'nested':
-          obj[el.name] = { type: el.type, value: el.value, children: iter(el.children) };
+          acc[el.name] = { type: el.type, value: el.value, children: iter(el.children) };
           break;
         case 'changed':
-          obj[el.name] = { type: el.type, firstValue: el.firstValue, secondValue: el.secondValue };
+          acc[el.name] = {
+            type: el.type,
+            firstValue: el.firstValue,
+            secondValue: el.secondValue,
+          };
           break;
         default:
           break;
       }
-    });
-
-    return obj;
+      return acc;
+    }, {});
+    return result;
   };
 
   return JSON.stringify(iter(data));
