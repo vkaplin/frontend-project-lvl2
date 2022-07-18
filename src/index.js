c063a183da674b3890bd493e0de7eb082d15484e
Kaplin Vladimir <hawk.hawk30@gmail.com>
2022-07-18 11:48:39 +0300

    выделил формирование отличий в отдтельный модуль

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import _ from 'lodash';
 import parsers from './parsers.js';
 import formater from './formaters/index.js';
-
-const getKeys = (obj1, obj2) => _.uniq(_.flatten([_.keys(obj1), _.keys(obj2)]));
-const getSortedKey = (obj1, obj2) => _.sortBy(getKeys(obj1, obj2));
+import getDifferens from './getDifferens.js';
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const file1 = parsers(filepath1);
@@ -11,37 +8,6 @@ const genDiff = (filepath1, filepath2, format = 'stylish') => {
   if (!file1 || !file2) {
     return null;
   }
-
-  const getDifferens = (obj1, obj2) => {
-    const keys = getSortedKey(obj1, obj2);
-    const resultObj = keys.map((key) => {
-      if (_.has(obj1, key) && _.has(obj2, key)) {
-        const value1 = obj1[key];
-        const value2 = obj2[key];
-
-        if (typeof (value1) === 'object' && typeof (value2) === 'object') {
-          return { name: key, type: 'nested', children: getDifferens(value1, value2) };
-        }
-        if (value1 === value2) {
-          return { name: key, type: 'unchanged', value: value1 };
-        }
-        return {
-          name: key,
-          type: 'changed',
-          firstValue: value1,
-          secondValue: value2,
-        };
-      }
-      if (_.has(obj1, key)) {
-        return { name: key, type: 'removed', value: obj1[key] };
-      }
-      if (_.has(obj2, key)) {
-        return { name: key, type: 'added', value: obj2[key] };
-      }
-      return null;
-    });
-    return resultObj;
-  };
 
   const differenceData = getDifferens(file1, file2);
   const result = formater(differenceData, format);
